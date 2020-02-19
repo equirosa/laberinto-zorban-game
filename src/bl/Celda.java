4458c0dbc906ad794e1d1ea97459a47076921c21
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-02-19 15:49:18 -0600

    Crear clase 'Celda'
    Es una clase simple que representa la celda de un tablero

---
+++ src/bl/Celda.java
@@ -0,0 +1,20 @@
+package bl;
+
+/* Esta es la clase base para las celdas del juego
+* NOTA: Puede ser mejor pasar esta clase a ser una clase abstracta*/
+public class Celda {
+    // Esto representa el número de la celda representada en el tablero del juego
+    private int numero;
+
+    public Celda(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+}
