c7f4e14aa4d016c533259cfbd56ac6117921d85f
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-03-27 14:14:06 -0600

    agregar singleton de tablero

--- src/bl/Tablero.java
+++ src/bl/Tablero.java
@@ -1,4 +1,21 @@
 package bl;
 
 public class Tablero {
+    private String gameName;
+    private static Tablero unicoTablero=null;
+
+    public static Tablero getTablero(String gameName) {
+        if (unicoTablero == null) {
+            unicoTablero = new Tablero(gameName);
+        }
+        return unicoTablero;
+    }
+
+    private Tablero(String pGameName) {
+        setGameName(pGameName);
+    }
+
+    public void setGameName(String pGameName) {
+        this.gameName = pGameName;
+    }
 }
