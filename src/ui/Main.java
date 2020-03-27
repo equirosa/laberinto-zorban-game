d48e80e3d8bf7519f824539e62492dbf78c5e7d6
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-03-27 14:33:12 -0600

    agregar prueba del singleton de tablero

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,8 +1,30 @@
 package ui;
 
+import bl.Tablero;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
 public class Main {
+    private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    private static PrintStream out = System.out;
 
     public static void main(String[] args){
-        
+        String opcion = null;
+        do {
+            out.println("Seleccione el nombre del nuevo juego.");
+            try {
+                String newGame = in.readLine();
+                Tablero t1 = Tablero.getTablero(newGame);
+                out.println("El nombre del tablero es: " + t1.getGameName() +
+                        "\nDesea hacerlo de nuevo?[Y/n]");
+                opcion = in.readLine();
+            } catch (IOException e) {
+                out.println("No se pudo leer.");
+                e.printStackTrace();
+            }
+        }while(opcion.toLowerCase().charAt(0) == 'y');
     }
 }
